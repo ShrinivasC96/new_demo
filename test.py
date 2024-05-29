4f7961534815bab1d0abbe3ea10a09cf30d4f3dc
shrinivas <shrinivas.chavan7387@gmail.com>
2024-05-29 14:00:24 +0530

    The program of sum of even numbers from 1 to 50 is added in test.py

--- test.py
+++ test.py
@@ -6,3 +6,13 @@ sum_of_odd = 0
 for i in range(1,50,2):
   sum_of_odd += i
 print("The sum of odd numbers from 1 to 50 is : ", sum_of_odd)  
+
+
+'''
+program to add sum of 1 to 50 even numbers
+'''
+sum_of_even = 0
+
+for i in range(2,51,2):
+  sum_of_even += i
+print("The sum of even numbers from 1 to 50 is : ", sum_of_even) 

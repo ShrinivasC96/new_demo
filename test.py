de543090e23dfe3176bacc719a3af79a76e76253
shrinivas <shrinivas.chavan7387@gmail.com>
2024-05-29 14:28:53 +0530

    Added table of 7 using for loop in test.py

--- test.py
+++ test.py
@@ -15,4 +15,14 @@ sum_of_even = 0
 
 for i in range(2,51,2):
   sum_of_even += i
-print("The sum of even numbers from 1 to 50 is : ", sum_of_even) 
+print("The sum of even numbers from 1 to 50 is : ", sum_of_even)
+
+'''
+program the nummber multiple of 7
+'''
+
+n = 7
+num = 0
+for i in range(1,11,1):
+  num = n * i
+  print("The multiple of 7 is :", num)  
